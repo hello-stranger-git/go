ebcdc3f49f66620566f639bd831dc42ae78d6457
xieyingcai <xieyingcai@91cyt.com>
2022-08-25 18:45:01 +0800

    添加gin和gorm框架

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Print(db)
 }
